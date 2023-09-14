ebc13695fa62e7b289383ed55bc18b261e4d70a5
Breanna Gernon <BGernon@my.gcu.edu>
2023-09-14 06:13:01 -0700

    Connected to Database

--- connect.php
+++ connect.php
@@ -2,9 +2,9 @@
 
 //Get Heroku ClearDB connection information
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
+$cleardb_server = "us-cdbr-east-06.cleardb.net";
+$cleardb_username = "b6429d201979f1";
+$cleardb_password = "$80a7d413";
 $cleardb_db = substr($cleardb_url["path"],1);
 
 $active_group = 'default';
