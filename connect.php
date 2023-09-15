aa43af4e0fed97e897df35335219fafd60de7a54
Breanna Gernon <BGernon@my.gcu.edu>
2023-09-15 15:13:48 -0700

    Connected to Database

--- connect.php
+++ connect.php
@@ -1,16 +1,6 @@
 <?php
 
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = "us-cdbr-east-06.cleardb.net";
-$cleardb_username = "b6429d201979f1";
-$cleardb_password = "$80a7d413";
-$cleardb_db = substr($cleardb_url["path"],1);
-
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$con=new mysqli('eyw6324oty5fsovx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'fi8j09p6dfcs6cmi', 'uuceawv8tsmqralz', 'z8m4k04kc7o8w1d1');
 
 if($con===false){
     die("ERROR: Could not connect. " .
