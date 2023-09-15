ae856f377edbf183ff903c37c981f83b10c02be8
Breanna Gernon <BGernon@my.gcu.edu>
2023-09-15 15:21:27 -0700

    Connected to Database

--- delete.php
+++ delete.php
@@ -3,7 +3,7 @@ include 'connect.php';
 if(isset($_GET['deleteid'])){
     $id=$_GET['deleteid'];
     
-    $sql="DELETE FROM `cst-323`.`candle_database` WHERE (`id` = $id);";
+    $sql="DELETE FROM `candle_database` WHERE (`id` = $id);";
     $result=mysqli_query($con, $sql);
     if($result){
         header('location:mainPage.php');
